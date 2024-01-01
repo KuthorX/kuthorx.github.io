22685296d0af8d474e1b219b0208ade862260e36
KuthorX <KuthorX@users.noreply.github.com>
2024-01-01 13:52:12 +0000

    deploy: KuthorX/KuthorXBlog@b81db2b6dffe04cdf789c44065215aef8a6be608

---
+++ apps/drink-water/precache-manifest.63984577f8dedaccb67ae5dfcc9c1a28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "18dd6a7a417899e8b067",
+    "url": "./static/js/main.21e9008e.chunk.js"
+  },
+  {
+    "revision": "8dd363c79cf661149ea7",
+    "url": "./static/js/2.979c286a.chunk.js"
+  },
+  {
+    "revision": "18dd6a7a417899e8b067",
+    "url": "./static/css/main.c4b456d2.chunk.css"
+  },
+  {
+    "revision": "8dd363c79cf661149ea7",
+    "url": "./static/css/2.1300e23a.chunk.css"
+  },
+  {
+    "revision": "07ace82329e12b310b757b656a7c56cf",
+    "url": "./index.html"
+  }
+];
---
+++ apps/memorysquares/precache-manifest.6397587707fe3dc152780e3fcf88189b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "4925d0952e4edc1d768d",
+    "url": "./static/js/main.b1bc8e9b.chunk.js"
+  },
+  {
+    "revision": "a6d27cca889cbfc7e586",
+    "url": "./static/js/2.72968f60.chunk.js"
+  },
+  {
+    "revision": "4925d0952e4edc1d768d",
+    "url": "./static/css/main.c3da0aa0.chunk.css"
+  },
+  {
+    "revision": "824fc766cc9eceabd0678448512b29aa",
+    "url": "./index.html"
+  }
+];
